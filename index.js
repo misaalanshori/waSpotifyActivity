0d5a6c2d5997b152202f872337e082f22511a32e
M Isa Al Anshori <isabungsu@gmail.com>
2023-08-24 18:05:32 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,100 @@
+import {
+  makeWASocket,
+  useMultiFileAuthState,
+} from "@whiskeysockets/baileys";
+import authSpotify from "./spotify.js";
+
+const spotifyApi = await authSpotify();
+
+function msToTime(duration) {
+  var seconds = parseInt((duration / 1000) % 60),
+    minutes = parseInt((duration / (1000 * 60)) % 60);
+  return minutes + ":" + (seconds < 10 ? "0" + seconds : seconds);
+}
+
+async function getSong() {
+  try {
+    const data = await spotifyApi.getMyCurrentPlaybackState();
+    if (!data.body || !data.body.item || data.body.is_playing == false) {
+      return null;
+    }
+    return {
+      name: data.body.item.name,
+      artists: data.body.item.artists,
+      album: data.body.item.album.name,
+      progress: data.body.progress_ms,
+      duration: data.body.item.duration_ms,
+      id: data.body.item.id,
+    };
+  } catch (error) {
+    console.log("SPOTIFY ERROR: ", error);
+    return null;
+  }
+}
+
+let lastSong = null;
+let lastUpdate = 0;
+async function updateStatusWithSongOrFallbackToRandomStatus(conn) {
+  const song = await getSong();
+  if (!song) {
+    if (lastSong) {
+      lastSong = null;
+    } else if (Date.now() - lastUpdate > 3600 * 1000) {
+      lastUpdate = Date.now();
+    } else {
+      return;
+    }
+    // if no song is playing, update status with random status
+    const randomStatus = [
+      "I am delusional",
+      "Currently listening to ABSOLUTELY NOTHING",
+      "Probably having a mental breakdown",
+    ];
+    await conn.updateProfileStatus(
+      randomStatus[Math.floor(Math.random() * randomStatus.length)]
+    );
+    lastUpdate = Date.now();
+    return;
+  }
+  if (song.id != lastSong || Date.now() - lastUpdate > 15 * 1000) {
+    // console.log("Updating!", song, lastSong, lastUpdate, Date.now(), Date.now() - lastUpdate)
+    // if song is playing, update status with song
+    const feat =
+      song.artists.length > 1
+        ? `(feat. ${song.artists
+            .slice(1)
+            .map((e) => e.name)
+            .join(", ")})`
+        : "";
+    const progress = `${msToTime(song.progress)}/${msToTime(song.duration)}`;
+    await conn.updateProfileStatus(
+      `lagi denger ${song.name} oleh ${song.artists[0].name} ${feat} (${progress})`
+    );
+    lastUpdate = Date.now();
+    lastSong = song.id;
+  }
+}
+
+async function ConnectWhatsapp() {
+  // utility function to help save the auth state in a single folder
+  // this function serves as a good guide to help write auth & key states for SQL/no-SQL databases, which I would recommend in any production grade system
+  const { state, saveCreds } = await useMultiFileAuthState("auth_info_baileys");
+
+  // will use the given state to connect
+  // so if valid credentials are available -- it'll connect without QR
+  const conn = makeWASocket({
+    auth: state,
+    printQRInTerminal: true,
+  });
+
+  conn.ev.on('creds.update', saveCreds);
+
+  // this will update the status every couple of seconds
+  setInterval(() => updateStatusWithSongOrFallbackToRandomStatus(conn), 2000);
+}
+
+await ConnectWhatsapp();
+
+while (true) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+}
---
+++ spotify.js
@@ -0,0 +1,84 @@
+import SpotifyWebApi from "spotify-web-api-node";
+import express from "express";
+import fs from "fs";
+import prompt from "prompt-sync";
+
+export default async function authSpotify() {
+  const scopes = ["user-read-playback-state"];
+  // load the credentials from a json file
+  const config = JSON.parse(fs.readFileSync("spotifyConfig.json"));
+  var spotifyApi = new SpotifyWebApi({
+    clientId: config.clientId,
+    clientSecret: config.clientSecret,
+    redirectUri: config.redirect + ":" + config.redirectPort,
+  });
+
+  async function getToken() {
+    var authorizeURL = spotifyApi.createAuthorizeURL(scopes, "poopsicle");
+    console.log(authorizeURL);
+
+    let spotifyCode = undefined;
+
+    if (config.headless) {
+      const url = prompt()("Enter the URL you were redirected to: ");
+      const parsedUrl = new URL(url);
+      spotifyCode = parsedUrl.searchParams.get("code");
+    } else {
+      const app = express();
+      const port = config.redirectPort;
+      let server = null;
+
+      app.get("/", (req, res) => {
+        if (req.query.code) {
+          spotifyCode = req.query.code;
+          res.send("Hello World!");
+          console.log("Code Received! Closing server");
+          server.close();
+        }
+      });
+
+      server = app.listen(port, () => {
+        console.log(`Code retriever listening on port ${port}`);
+      });
+
+      while (spotifyCode == undefined) {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+    }
+
+    const accessToken = await spotifyApi.authorizationCodeGrant(spotifyCode);
+
+    return {
+      accessToken: accessToken.body["access_token"],
+      refreshToken: accessToken.body["refresh_token"],
+    };
+  }
+
+  // load stored token in file
+  let tokens;
+  try {
+    tokens = JSON.parse(fs.readFileSync("spotifyTokens.json").toString());
+    spotifyApi.setAccessToken(tokens.accessToken);
+    spotifyApi.setRefreshToken(tokens.refreshToken);
+    // try to print username
+    const data = await spotifyApi.getMe();
+    console.log("Spotify Logged in as", data.body.display_name);
+  } catch (error) {
+    tokens = await getToken();
+    fs.writeFileSync("spotifyTokens.json", JSON.stringify(tokens));
+    spotifyApi.setAccessToken(tokens.accessToken);
+    spotifyApi.setRefreshToken(tokens.refreshToken);
+    // try to print username
+    const data = await spotifyApi.getMe();
+    console.log("Spotify Logged in as", data.body.display_name);
+  }
+
+  // refresh token on an interval
+  setInterval(async () => {
+    const data = await spotifyApi.refreshAccessToken();
+    spotifyApi.setAccessToken(data.body["access_token"]);
+    tokens.accessToken = data.body["access_token"];
+    fs.writeFileSync("spotifyTokens.json", JSON.stringify(tokens));
+  }, 3000 * 1000); // refresh every 3ks
+  return spotifyApi;
+}
